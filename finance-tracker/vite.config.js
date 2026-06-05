00df470bc99b78e9e5d09323bf1950e178e4960a
Panagiotis2929 <rpkyranas@gmail.com>
2026-06-06 02:28:57 +0300

    update base path for gh-pages

--- finance-tracker/vite.config.js
+++ finance-tracker/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/finance-tracker/', // ΠΡΟΣΟΧΗ: Βάλε το όνομα του repo σου εδώ
+  plugins: [react()]
 })
