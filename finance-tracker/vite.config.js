94ead3c9ac7758fe46ab80c27fb7a9112ec7962e
Panagiotis2929 <rpkyranas@gmail.com>
2026-06-06 02:56:34 +0300

    update build for manual deployment

--- finance-tracker/vite.config.js
+++ finance-tracker/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // Αυτό είναι το πιο σημαντικό για να βρει τα αρχεία το GitHub
+  base: '/finance-tracker/', 
   plugins: [react()]
 })
