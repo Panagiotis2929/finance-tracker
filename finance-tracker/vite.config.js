1c1b4d46439459eb165bcee20a9074a294d31a9c
Panagiotis2929 <rpkyranas@gmail.com>
2026-06-06 02:42:48 +0300

    fix base path for deployment

--- finance-tracker/vite.config.js
+++ finance-tracker/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
-  base: './', 
+  base: './', // Αυτό είναι το πιο σημαντικό για να βρει τα αρχεία το GitHub
   plugins: [react()]
 })
