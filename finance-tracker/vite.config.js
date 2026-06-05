23797f33c8feb8b1754d96ba65f7bb20c7b0e835
Panagiotis2929 <rpkyranas@gmail.com>
2026-06-06 02:35:11 +0300

    update project settings

--- finance-tracker/vite.config.js
+++ finance-tracker/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/finance-tracker/', // ΠΡΟΣΟΧΗ: Βάλε το όνομα του repo σου εδώ
+
+  base: './', 
   plugins: [react()]
 })
